592265d7a21d0d1858d9a532d08d3262a75ef4f5
caofeilong <feilong.cao@qq.com>
2019-07-05 23:23:56 +0800

    test

---
+++ index.js
@@ -0,0 +1,54 @@
+//"use strict";
+
+function clickFn(e) {
+  console.info(this, e, this, window.event);
+}
+
+function onload() {
+  document.getElementById("123").addEventListener(
+    "click",
+    function(e) {
+      console.info(this, e);
+    },
+    true
+  );
+}
+// this 指向的问题
+var lang = 1;
+
+function fn1() {
+  console.info(this.lang);
+}
+
+var obj = {
+  lang: 2,
+  method1: function(fn) {
+    fn();
+    fn.call(this);
+    arguments[0]();
+  },
+  mtehod2: function(fn) {
+    document.addEventListener(
+      "click",
+      function() {
+        fn();
+      },
+      true
+    );
+  }
+};
+
+obj.method1(fn1);
+obj.mtehod2(fn1);
+
+
+//变量提升问题
+var a = 10;
+function test() {
+  if (!a) {
+    var a = 100;
+  }
+  console.info(a);
+}
+
+test()
